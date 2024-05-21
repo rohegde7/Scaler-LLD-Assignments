bc5385a76b02d00b8c84bcf0428c6f932208a28a
rvd <hegde.rohit7@gmail.com>
2024-05-21 05:37:13 +0530

    undertanding Thread.join()

--- src/threads/Demo.java
+++ src/threads/Demo.java
@@ -10,3 +10,27 @@ public class Demo {
         System.out.println(new ThreadGroup("Helllo").getMaxPriority());
     }
 }
+
+ class JoinExample {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            System.out.println("Thread t1 is running");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread t1 has finished");
+        });
+
+        t1.start();
+
+        try {
+            t1.join();  // Waits for t1 to finish
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Main thread continues after t1 has finished");
+    }
+}
