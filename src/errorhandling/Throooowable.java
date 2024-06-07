6c1a0736efa99ec04ffcc80b426e698614ae3b31
rvd <hegde.rohit7@gmail.com>
2024-06-08 01:40:58 +0530

    checking if after overriding a func can I thowr an exception only in the child methid

--- src/errorhandling/Throooowable.java
+++ src/errorhandling/Throooowable.java
@@ -2,7 +2,10 @@ package errorhandling;
 
 public class Throooowable {
 
-
+    public static void main(String[] args) {
+        Parent p = new Child();
+        p.print(10);
+    }
 }
 
 class Something {
@@ -13,5 +16,18 @@ class Something {
 
     void some2() throws RuntimeException {
 
+    }
+}
+
+class Parent {
+    void print(int i) {
+
+    }
+}
+
+class Child extends Parent {
+    @Override
+    void print(int i) throws RuntimeException {
+        System.out.println("Hello Childred");
     }
 }
