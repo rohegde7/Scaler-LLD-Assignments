6f51b67cb03df1586dd1fee426dfe83d7b459f64
rvd <hegde.rohit7@gmail.com>
2024-06-08 00:43:56 +0530

    trying to throw Error and RuntimeException and checking its parent type

---
+++ src/errorhandling/Throooowable.java
@@ -0,0 +1,17 @@
+package errorhandling;
+
+public class Throooowable {
+
+
+}
+
+class Something {
+
+    void some() throws Error {
+
+    }
+
+    void some2() throws RuntimeException {
+
+    }
+}
