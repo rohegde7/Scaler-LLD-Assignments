9ebc86361a789383b36707c23967f9298ce959f7
rvd <hegde.rohit7@gmail.com>
2024-06-14 04:18:20 +0530

    Singleton FileBasedConfigurationManagerImpl problem

---
+++ src/singleton/FileBasedConfigurationManagerImpl.java
@@ -0,0 +1,67 @@
+package singleton;
+
+public class FileBasedConfigurationManagerImpl extends FileBasedConfigurationManager {
+
+    private static volatile FileBasedConfigurationManager configManager = null;
+
+    private FileBasedConfigurationManagerImpl() {
+
+    }
+
+    @Override
+    public String getConfiguration(String key) {
+        return properties.getProperty(key);
+//        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+    }
+
+    @Override
+    public <T> T getConfiguration(String key, Class<T> type) {
+        Object value = properties.getProperty(key);
+
+        if(value == null) return null;
+
+        if(type == Integer.class) {
+            return type.cast(Integer.parseInt(value.toString()));
+        }
+
+        return (type.cast(value));
+//        return (T) properties.getProperty(key);
+//        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+    }
+
+    @Override
+    public void setConfiguration(String key, String value) {
+        properties.setProperty(key, value);
+    }
+
+    @Override
+    public <T> void setConfiguration(String key, T value) {
+        properties.setProperty(key, value.toString());
+    }
+
+    @Override
+    public void removeConfiguration(String key) {
+        properties.remove(key);
+    }
+
+    @Override
+    public void clear() {
+        properties.clear();
+    }
+
+    public static FileBasedConfigurationManager getInstance() {
+        if (configManager == null) {
+            synchronized (FileBasedConfigurationManagerImpl.class) {
+                if (configManager == null) {
+                    configManager = new FileBasedConfigurationManagerImpl();
+                }
+            }
+        }
+
+        return configManager;
+    }
+
+    public static void resetInstance() {
+        configManager = null;
+    }
+}
