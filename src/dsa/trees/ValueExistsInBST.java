36d1eebda1f9d75e3dd2730d961204bc52984bed
rvd <hegde.rohit7@gmail.com>
2024-08-26 13:49:59 +0530

    value exists in BST done

--- src/dsa/trees/ValueExistsInBST.java
+++ src/dsa/trees/ValueExistsInBST.java
@@ -1,2 +1,17 @@
-package dsa.trees;public class ValueExistsInBST {
+package dsa.trees;
+
+public class ValueExistsInBST {
+
+    public int solve(TreeNode A, int B) {
+
+        if (A == null) return 0;
+
+        if (A.val == B) return 1;
+
+
+        if(solve(A.left, B) == 1) return 1;
+        if(solve(A.right, B) == 1) return 1;
+
+        return 0;
+    }
 }
